975882957d277712f13cd9c99532d1b38c2f8e76
Martin Otero <martin_otero@hotmail.com>
2023-10-06 11:27:29 -0300

    CardDetailsFixConditional

--- client/src/app/detail/page.tsx
+++ client/src/app/detail/page.tsx
@@ -1,27 +1,32 @@
 "use client";
 
-import { useSearchParams } from 'next/navigation';
-import Link from 'next/link';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import Card from '../types/card';
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Card from "../types/card";
 
 const CardDetail: React.FC = () => {
-
-  const searchParams = useSearchParams()
-  const id = searchParams.get('id')
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
   const [card, setCard] = useState<Card | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchCard = async () => {
       if (id) {
         try {
-          const response = await axios.get(`https://api.scryfall.com/cards/${id}`);
+          const response = await axios.get(
+            `https://api.scryfall.com/cards/${id}`
+          );
           setCard(response.data);
+          setLoading(false);
           console.log("Card fetched successfully");
-          console.log(response.data);
         } catch (error) {
-          console.error('Error fetching card details:', error);
+          setError("Error fetching card details");
+          setLoading(false);
+          console.error("Error fetching card details:", error);
         }
       }
     };
@@ -30,43 +35,48 @@ const CardDetail: React.FC = () => {
   }, [id]);
 
   const styles = {
-    container: 'bg-white p-8 rounded-lg shadow-md',
-    cardImage: 'w-full h-96 object-contain rounded-lg',
-    cardTitle: 'text-2xl font-semibold mt-4',
-    backButton: 'mt-4 text-blue-500 hover:underline',
+    container: "bg-white p-8 rounded-lg shadow-md",
+    cardImageContainer: "flex justify-start space-x-10 ",
+    cardImage: "w-[250px] h-[600px] object-contain rounded-lg",
+    cardTitle: "text-2xl font-semibold mt-4",
+    backButton: "mt-4 text-blue-500 hover:underline",
   };
 
   return (
     <div className={styles.container}>
-      {card ? (
+      {loading ? (
+        <p>Cargando...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : card ? (
         <>
-          <div className="flex">
-          {card.card_faces ? (
-            card.card_faces.map((face, index) => (
+          <div className={styles.cardImageContainer}>
+            {card.card_faces && card.card_faces[0].image_uris?.normal ? (
+              card.card_faces.map((face, index) => (
+                <img
+                  key={index}
+                  src={face.image_uris?.large}
+                  alt={card.name}
+                  className={styles.cardImage}
+                />
+              ))
+            ) : (
               <img
-                key={index}
-                src={face.image_uris?.normal}
+                src={card.image_uris?.large}
                 alt={card.name}
                 className={styles.cardImage}
               />
-            ))
-          ) : (
-            <img
-              src={card.image_uris?.normal}
-              alt={card.name}
-              className={styles.cardImage}
-            />
-          )}
-        </div>
+            )}
+          </div>
           <h1 className={styles.cardTitle}>{card.name}</h1>
           {/* Mostrar más detalles de la carta aquí */}
           <Link href="/">
-            <button className={styles.backButton}>Volver a la página de inicio</button>
+            <button className={styles.backButton}>
+              Volver a la página de inicio
+            </button>
           </Link>
         </>
-      ) : (
-        <p>Cargando...</p>
-      )}
+      ) : null}
     </div>
   );
 };
