913ee618fe07ac70fbd221d8635d62217f2f3bbe
Martin Otero <martin_otero@hotmail.com>
2023-10-10 12:10:19 -0300

    CardDetailsUniversal

--- client/src/app/detail/page.tsx
+++ client/src/app/detail/page.tsx
@@ -39,7 +39,7 @@ const CardDetail: React.FC = () => {
     cardImageContainer: "flex justify-start space-x-10 ",
     cardImage: "w-[400px] object-contain rounded-lg",
     cardInfo:
-      "flex flex-col space-y-2 max-w-[400px] ml-5 mr-10 p-5 border border-gray-300 shadow-lg rounded ",
+      "flex flex-col space-y-2 max-w-[400px] w-[400px] ml-5 mr-10 p-5 border border-gray-300 shadow-lg rounded ",
     cardTitle: "text-2xl font-semibold mt-4",
     backButton: "mt-4 text-blue-500 hover:underline",
   };
@@ -62,6 +62,7 @@ const CardDetail: React.FC = () => {
                     className={styles.cardImage}
                   />
                   <div className={styles.cardInfo}>
+                    <h2 className={styles.cardTitle}>{face.name}</h2>
                     <p>
                       Mana cost: {face.mana_cost ? face.mana_cost : "no posee"}
                     </p>
@@ -80,27 +81,44 @@ const CardDetail: React.FC = () => {
                   alt={card.name}
                   className={styles.cardImage}
                 />
-                <div className={styles.cardInfo}>
-                  <p>
-                    Mana cost: {card.mana_cost ? card.mana_cost : "no posee"}
-                  </p>
-                  <p>Card Type: {card.type_line}</p>
-                  <div>
-                    {card.oracle_text ? (
+                {card.oracle_text ? (
+                  <div className={styles.cardInfo}>
+                    <h2 className={styles.cardTitle}>{card.name}</h2>
+                    <p>
+                      Mana cost: {card.mana_cost ? card.mana_cost : "no posee"}
+                    </p>
+                    <p>Card Type: {card.type_line}</p>
+                    <div>
                       <p>{card.oracle_text}</p>
-                    ) : (
-                      <p>{card.card_faces[0].oracle_text}</p>
+                    </div>
+
+                    {card.power && card.toughness && (
+                      <p>{`${card.power}/${card.toughness}`}</p>
                     )}
                   </div>
+                ) : (
+                  card.card_faces?.map((face, index) => (
+                    <div key={index} className={styles.cardInfo}>
+                      <h2 className={styles.cardTitle}>{face.name}</h2>
+                      <p>
+                        Mana cost:{" "}
+                        {face.mana_cost ? face.mana_cost : "no posee"}
+                      </p>
+                      <p>Card Type: {face.type_line}</p>
+                      <div>
+                        <p>{face.oracle_text}</p>
+                      </div>
 
-                  {card.power && card.toughness && (
-                    <p>{`${card.power}/${card.toughness}`}</p>
-                  )}
-                </div>
+                      {face.power && face.toughness && (
+                        <p>{`${face.power}/${face.toughness}`}</p>
+                      )}
+                    </div>
+                  ))
+                )}
               </div>
             )}
           </div>
-          <h1 className={styles.cardTitle}>{card.name}</h1>
+
           {/* Mostrar más detalles de la carta aquí */}
           <Link href="/">
             <button className={styles.backButton}>
